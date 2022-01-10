ddd5690608db7b72a64ba1c6065ddc067e855fc4
Paul Taiwo <ayopaulot@gmail.com>
2022-01-10 01:02:03 +0100

    add google font

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -11,66 +11,9 @@ const GlobalStyle = createGlobalStyle`
     --c-white: #FFFFFF;
   }
 
-  @font-face {
-    font-family: "NeurialGrotesk-Light";
-    src: url(/assets/fonts/NeurialGrotesk-Light.otf) format('opentype');
-    font-weight: 300;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-LightItalic";
-    src: url(/assets/fonts/NeurialGrotesk-LightItalic.otf) format('opentype');
-    font-weight: 300;
-    font-style: italic;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk";
-    src: url(/assets/fonts/NeurialGrotesk-Regular.otf) format('opentype');
-    font-weight: 400;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-Italic";
-    src: url(/assets/fonts/NeurialGrotesk-Italic.otf) format('opentype');
-    font-weight: 400;
-    font-style: italic;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-Medium";
-    src: url(/assets/fonts/NeurialGrotesk-Medium.otf) format('opentype');
-    font-weight: 500;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-MediumItalic";
-    src: url(/assets/fonts/NeurialGrotesk-MediumItalic.otf) format('opentype');
-    font-weight: 500;
-    font-style: italic;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-Bold";
-    src: url(/assets/fonts/NeurialGrotesk-Bold.otf) format('opentype');
-    font-weight: 700;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-BoldItalic";
-    src: url(/assets/fonts/NeurialGrotesk-BoldItalic.otf) format('opentype');
-    font-weight: 700;
-    font-style: italic;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-Extrabold";
-    src: url(/assets/fonts/NeurialGrotesk-Extrabold.otf) format('opentype');
-    font-weight: 800;
-  }
-  @font-face {
-    font-family: "NeurialGrotesk-ExtraboldItalic";
-    src: url(/assets/fonts/NeurialGrotesk-ExtraboldItalic.otf) format('opentype');
-    font-weight: 800;
-    font-style: italic;
-  }
-
   html,body {
     overflow-x: hidden;
     background-color: var(--c-black1);
-    font-family: "NeurialGrotesk", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
     font-size: 16px;
     color: var(--c-white);
 		background: red;
@@ -80,9 +23,8 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-			'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-			sans-serif;
+		font-family: 'Nunito', sans-serif !important;
+    font-weight: 300;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
   }
