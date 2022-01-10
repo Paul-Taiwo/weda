b205f82311eb7b1468618874cb1a63e4d834163c
Paul Taiwo <ayopaulot@gmail.com>
2022-01-10 18:33:52 +0100

    weather details pop up card

---
+++ src/components/PopUp/index.js
@@ -0,0 +1,87 @@
+/* eslint-disable react/prop-types */
+import React, { forwardRef } from "react";
+import { ArrowMaxIcon, ArrowMinIcon, HumidityIcon, Loader, WindIcon } from "components/VectorIcons";
+import { StyledPopUp } from "./styles";
+
+const PopUp = forwardRef(({ status, data, WeatherIcon }, ref) => {
+    return (
+        <StyledPopUp ref={ref}>
+            {status === "loading" && (
+                <div className="d-flex flex-column align-items-center justify-content-center">
+                    <Loader height="25px" />
+                    <p className="m-0 mt-2">Loading Data</p>
+                </div>
+            )}
+
+            {status === "success" && (
+                <>
+                    <div className="d-flex mb-2">
+                        <div className="w-50 mt-3">
+                            <h4 className="city-name m-0">{data.name}</h4>
+                            <p className="city-temperature mt-2 mb-0">{data.main.temp}°</p>
+                            <div className="city-weather__condition">
+                                <p className="m-0">{data.weather[0].description}</p>
+                            </div>
+                        </div>
+
+                        <div className="w-50 d-flex align-items-center justify-content-center ps-2">
+                            {WeatherIcon}
+                        </div>
+                    </div>
+
+                    <div className="d-flex mb-3">
+                        <div className="w-50 d-flex flex-column align-items-center">
+                            <ArrowMinIcon />
+
+                            <span className="city-temperature__min mt-2">
+                                {data.main.temp_min}°
+                            </span>
+                            <span className="min__text">Min</span>
+                        </div>
+
+                        <div className="w-50 d-flex flex-column align-items-center">
+                            <ArrowMaxIcon />
+
+                            <span className="city-temperature__max mt-2">
+                                {data.main.temp_max}°
+                            </span>
+                            <span className="max__text">Max</span>
+                        </div>
+                    </div>
+
+                    <div className="d-flex align-items-center mb-2">
+                        <div className="w-50 border-right text-center">
+                            <p className="m-0">
+                                <span className="me-2">
+                                    <WindIcon />
+                                </span>
+                                Wind
+                            </p>
+                        </div>
+
+                        <div className="w-50 text-center">
+                            <p className="m-0">{data.wind.speed} meter/sec</p>
+                        </div>
+                    </div>
+
+                    <div className="d-flex align-items-center">
+                        <div className="w-50 border-right text-center">
+                            <p className="m-0">
+                                <span className="me-2">
+                                    <HumidityIcon />
+                                </span>
+                                Hum
+                            </p>
+                        </div>
+
+                        <div className="w-50 text-center">
+                            <p className="m-0">{data.main.humidity}%</p>
+                        </div>
+                    </div>
+                </>
+            )}
+        </StyledPopUp>
+    );
+});
+
+export default PopUp;
---
+++ src/components/PopUp/styles.js
@@ -0,0 +1,82 @@
+/* eslint-disable import/prefer-default-export */
+/* -------------------------------------------------------------------------- */
+/*                             External Dependency                            */
+/* -------------------------------------------------------------------------- */
+import styled from "styled-components";
+import { Popup } from "react-leaflet";
+
+const StyledPopUp = styled(Popup)`
+    .border-right {
+        border-right: 1px solid white;
+    }
+
+    .leaflet-popup-tip {
+        background: linear-gradient(180deg, #014351 0, #41747e 100%);
+    }
+
+    .leaflet-popup-content {
+        min-width: 250px;
+        font-family: "Nunito", sans-serif !important;
+
+        &-wrapper {
+            background: linear-gradient(180deg, #014351 0, #41747e 100%);
+            color: var(--c-white);
+        }
+
+        .city {
+            &-name {
+                text-transform: uppercase;
+                font-size: 1.2rem;
+                letter-spacing: 0.1rem;
+            }
+
+            &-temperature {
+                font-size: 2.4rem;
+                font-weight: 500;
+
+                &__min,
+                &__max {
+                    font-size: 1.15rem;
+                }
+            }
+
+            &-weather__condition {
+                background-color: #76caac;
+                text-align: center;
+                border-radius: 50px;
+                padding: 4px 2px;
+
+                p {
+                    white-space: nowrap;
+                    text-transform: capitalize;
+                    font-size: 0.75rem;
+                }
+            }
+        }
+
+        .arrow {
+            height: 1.25rem;
+            margin: auto;
+
+            &-max__icon {
+                margin-bottom: -0.65rem;
+            }
+        }
+
+        .min__text,
+        .max__text {
+            font-size: 0.9375rem;
+            text-align: center;
+        }
+
+        .min__text {
+            color: #00ff9b;
+        }
+
+        .max__text {
+            color: #ff0070;
+        }
+    }
+`;
+
+export { StyledPopUp };
