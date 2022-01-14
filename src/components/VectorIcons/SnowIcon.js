03c8a0bdc45267717a2991ee6cb3339b3fcb025a
Paul Taiwo <ayopaulot@gmail.com>
2022-01-14 11:48:43 +0100

    add new weather icons

---
+++ src/components/VectorIcons/SnowIcon.js
@@ -0,0 +1,298 @@
+/* -------------------------------------------------------------------------- */
+/*                             External Dependency                            */
+/* -------------------------------------------------------------------------- */
+import React from "react";
+
+const SnowIcon = () => {
+    return (
+        <svg
+            width={424}
+            height={126}
+            className="w-100"
+            viewBox="0 0 424 368"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M137.044 225.535L203.727 332.228"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M153.131 251.749L159.652 221.297"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M153.132 251.749L122.309 244.636"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M188.695 308.652L218.926 316.135"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M188.696 308.652L181.583 339.475"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M199.937 223.934L140.879 335.03"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M185.278 250.973L214.911 241.394"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M185.278 250.973L176.026 220.723"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M153.781 310.224L162.416 340.146"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M153.781 310.224L123.531 319.476"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M232.769 277.599L107.028 282.002"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M202.023 278.425L225.135 299.298"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M202.022 278.425L223.594 255.288"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M134.962 280.773L113.366 303.211"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M134.961 280.773L111.824 259.201"
+                stroke="#42A0F0"
+                strokeWidth={9}
+                strokeLinecap="round"
+            />
+            <path
+                d="M303.38 292.949L278.008 349.928"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M297.063 306.827L311.387 301.066"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M297.063 306.827L291.444 292.186"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M283.531 337.215L288.833 351.715"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M283.531 337.215L268.891 342.834"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M321.469 318.356L259.438 324.872"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M306.291 319.824L318.442 329.349"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M306.292 319.824L316.161 307.638"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M273.208 323.3L263.302 335.141"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M273.208 323.3L261.022 313.43"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M308.51 346.724L271.851 296.261"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M299.649 334.315L297.476 349.6"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M299.649 334.315L315.138 336.769"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M280.098 307.401L264.89 304.743"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M280.098 307.401L282.552 291.913"
+                stroke="#42A0F0"
+                strokeWidth="6.00313"
+                strokeLinecap="round"
+            />
+            <path
+                d="M108.148 214.763C162.548 214.763 206.648 170.439 206.648 115.763C206.648 61.0868 162.548 16.7629 108.148 16.7629C53.7484 16.7629 9.64844 61.0868 9.64844 115.763C9.64844 170.439 53.7484 214.763 108.148 214.763Z"
+                fill="#FA9E42"
+            />
+            <g filter="url(#filter0_f_130_370)">
+                <path
+                    d="M101.03 101.684C133.767 84.2964 152.077 54.7096 141.928 35.6004C131.778 16.4912 97.0115 15.0957 64.2749 32.4835C31.5383 49.8714 13.228 79.4581 23.3778 98.5674C33.5275 117.677 68.2938 119.072 101.03 101.684Z"
+                    fill="#F8E36F"
+                />
+            </g>
+            <g filter="url(#filter1_d_130_370)">
+                <path
+                    d="M293.345 285.76H101.913C83.3426 285.746 65.4976 278.549 52.1137 265.675C38.7298 252.801 30.8447 235.249 30.1092 216.693C29.3737 198.137 35.8448 180.016 48.1677 166.124C60.4906 152.231 77.71 143.644 96.221 142.16C98.8608 123.52 107.278 106.177 120.289 92.5699C133.299 78.9632 150.248 69.7779 168.752 66.3063C187.255 62.8347 206.381 65.2515 223.439 73.2167C240.497 81.1818 254.628 94.2945 263.845 110.71C280.75 104.858 299.015 104.174 316.311 108.746C333.606 113.318 349.148 122.938 360.953 136.379C372.759 149.82 380.293 166.473 382.594 184.213C384.896 201.954 381.861 219.978 373.877 235.987C369.192 250.45 360.045 263.058 347.749 272C335.453 280.942 320.641 285.759 305.437 285.76H293.345Z"
+                    fill="url(#paint0_radial_130_370)"
+                    fillOpacity="0.8"
+                />
+                <path
+                    d="M305.437 284.76C313.156 284.76 320.746 283.527 327.996 281.096C335.006 278.745 341.587 275.315 347.557 270.9C353.458 266.536 358.631 261.308 362.932 255.362C367.287 249.342 370.649 242.72 372.925 235.679L372.949 235.607L372.983 235.539C376.01 229.491 378.341 223.093 379.911 216.523C381.526 209.763 382.345 202.777 382.345 195.76C382.345 189.72 381.737 183.685 380.537 177.822C379.368 172.108 377.623 166.488 375.352 161.117C373.121 155.844 370.36 150.758 367.146 146C363.962 141.287 360.305 136.855 356.278 132.827C352.25 128.799 347.818 125.143 343.105 121.959C338.347 118.744 333.261 115.984 327.988 113.753C322.617 111.482 316.997 109.737 311.283 108.568C305.42 107.368 299.384 106.76 293.345 106.76C283.346 106.76 273.531 108.405 264.172 111.651L263.383 111.925L262.973 111.196C255.357 97.6542 244.279 86.2816 230.937 78.3078C224.189 74.2752 216.94 71.159 209.391 69.0457C201.602 66.8655 193.512 65.76 185.345 65.76C174.361 65.76 163.638 67.737 153.475 71.6361C143.657 75.4027 134.65 80.8437 126.705 87.808C118.806 94.7322 112.251 102.902 107.221 112.091C102.069 121.505 98.7006 131.667 97.2113 142.295L97.0998 143.09L96.299 143.153C87.3408 143.854 78.7219 146.213 70.6818 150.163C62.8694 154.002 55.8932 159.194 49.947 165.594C37.7289 178.747 31 195.883 31 213.847C31 223.42 32.8748 232.707 36.5723 241.449C38.3495 245.651 40.5491 249.703 43.1102 253.494C45.6472 257.249 48.5607 260.78 51.7699 263.99C54.9791 267.199 58.5104 270.112 62.2655 272.649C66.0565 275.21 70.109 277.41 74.3106 279.187C83.0525 282.885 92.3391 284.76 101.913 284.76H293.345H305.437ZM305.437 285.76H293.345H101.913C62.1966 285.76 30 253.563 30 213.847C30 176.046 59.1646 145.057 96.221 142.156C102.35 98.4174 139.917 64.76 185.345 64.76C219.048 64.76 248.422 83.2831 263.845 110.706C273.087 107.501 283.012 105.76 293.345 105.76C343.051 105.76 383.345 146.054 383.345 195.76C383.345 210.219 379.936 223.881 373.877 235.987C364.54 264.869 337.427 285.76 305.437 285.76Z"
+                    fill="url(#paint1_linear_130_370)"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_f_130_370"
+                    x="0.578125"
+                    y="0.303345"
+                    width="164.149"
+                    height="133.561"
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                >
+                    <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="BackgroundImageFix"
+                        result="shape"
+                    />
+                    <feGaussianBlur stdDeviation={10} result="effect1_foregroundBlur_130_370" />
+                </filter>
+                <filter
+                    id="filter1_d_130_370"
+                    x={10}
+                    y="36.76"
+                    width="413.346"
+                    height={281}
+                    filterUnits="userSpaceOnUse"
+                    colorInterpolationFilters="sRGB"
+                >
+                    <feFlood floodOpacity={0} result="BackgroundImageFix" />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset dx={10} dy={2} />
+                    <feGaussianBlur stdDeviation={15} />
+                    <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 0.501961 0 0 0 0 0.501961 0 0 0 0 0.501961 0 0 0 0.302 0"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in2="BackgroundImageFix"
+                        result="effect1_dropShadow_130_370"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="effect1_dropShadow_130_370"
+                        result="shape"
+                    />
+                </filter>
+                <radialGradient
+                    id="paint0_radial_130_370"
+                    cx={0}
+                    cy={0}
+                    r={1}
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(313.568 165.869) rotate(43.4125) scale(147.118 159.643)"
+                >
+                    <stop stopColor="white" stopOpacity="0.604" />
+                    <stop offset="0.0001" stopColor="white" stopOpacity="0.95" />
+                    <stop offset={1} stopColor="white" stopOpacity="0.95" />
+                </radialGradient>
+                <linearGradient
+                    id="paint1_linear_130_370"
+                    x1="101.852"
+                    y1="111.82"
+                    x2="340.352"
+                    y2="285.82"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.1" />
+                    <stop offset={1} stopColor="white" stopOpacity={0} />
+                </linearGradient>
+            </defs>
+        </svg>
+    );
+};
+
+export default SnowIcon;
--- src/components/VectorIcons/index.js
+++ src/components/VectorIcons/index.js
@@ -3,10 +3,29 @@ import ArrowMaxIcon from "./ArrowMaxIcon";
 import WindIcon from "./WindIcon";
 import HumidityIcon from "./HumidityIcon";
 import SunnyIcon from "./SunnyIcon";
+import SunnyNightIcon from "./SunnyNightIcon";
 import CloudyIcon from "./CloudyIcon";
+import FewCloudsIcon from "./FewCloudsIcon";
+import FewCloudsNightIcon from "./FewCloudsNightIcon";
+import ScatteredCloudsIcon from "./ScatteredCloudsIcon";
+import BrokenCloudsIcon from "./BrokenCloudsIcon";
+import SnowIcon from "./SnowIcon";
+import SnowNightIcon from "./SnowNightIcon";
 import RainyIcon from "./RainyIcon";
+import LightRainIcon from "./LightRainIcon";
+import LightRainNightIcon from "./LightRainNightIcon";
+import HeavyRainIcon from "./HeavyRainIcon";
+import HeavyRainNightIcon from "./HeavyRainNightIcon";
+import FreezingRainIcon from "./FreezingRainIcon";
+import ShowersIcon from "./ShowersIcon";
+import ShowersNightIcon from "./ShowersNightIcon";
 import HazeIcon from "./HazeIcon";
+import HazeNightIcon from "./HazeNightIcon";
 import StormIcon from "./StormIcon";
+import ThunderStormIcon from "./ThunderStormIcon";
+import ThunderStormNightIcon from "./ThunderStormNightIcon";
+import ThunderStormRainIcon from "./ThunderStormRainIcon";
+import ThunderStormRainNightIcon from "./ThunderStormRainNightIcon";
 import ArrowUpCircleIcon from "./ArrowUpCircleIcon";
 import Loader from "./Loader";
 
@@ -16,10 +35,29 @@ export {
     HumidityIcon,
     WindIcon,
     SunnyIcon,
+    SunnyNightIcon,
     CloudyIcon,
+    FewCloudsIcon,
+    FewCloudsNightIcon,
+    ScatteredCloudsIcon,
+    BrokenCloudsIcon,
     RainyIcon,
+    LightRainIcon,
+    LightRainNightIcon,
+    HeavyRainIcon,
+    HeavyRainNightIcon,
+    FreezingRainIcon,
+    ShowersIcon,
+    ShowersNightIcon,
     HazeIcon,
+    HazeNightIcon,
     StormIcon,
+    ThunderStormIcon,
+    ThunderStormNightIcon,
+    ThunderStormRainIcon,
+    ThunderStormRainNightIcon,
+    SnowIcon,
+    SnowNightIcon,
     ArrowUpCircleIcon,
     Loader,
 };
